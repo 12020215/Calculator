b5af9d44b8a0b2f7a88711e9b391a4c63cecaa39
okamotosoya <tosso@Mac.lan>
2025-06-03 21:20:19 +0900

    Update index.html and routing adjustments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator/Calculator';
 import Exchangerate from './components/Exchangerate/Exchangerate';
 
